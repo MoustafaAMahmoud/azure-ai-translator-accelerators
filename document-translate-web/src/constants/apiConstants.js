e3584c1f5bf5ae19447e6f1dd1db66ae94235a3b
Mostafa Mahmoud <MustafaAlaa.Mohamed@gmail.com>
2024-08-15 22:48:36 +0100

    Update apiConstants.js

--- document-translate-web/src/constants/apiConstants.js
+++ document-translate-web/src/constants/apiConstants.js
@@ -1,5 +1,5 @@
-export const API_KEY = "111111111111111111111111111111111111"; // Replace with your API key
-export const BASE_URL = "https://APIm_NAME.azure-api.net/translation-service"; // Replace with your API Management URL
+export const API_KEY = "75a2bc4375b244159e0505563e322d4d"; // Replace with your API key
+export const BASE_URL = "https://translator-apim-duwih.azure-api.net/translation-service"; // Replace with your API Management URL
 export const GET_LOGS_API = (date) => `${BASE_URL}/get_logs_by_date?date=${date}`;
 export const GET_ALL_LOGS = `${BASE_URL}/get_all_logs`;
 export const UPLOAD_API = `${BASE_URL}/upload_file`;
